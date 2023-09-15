ccda5412e9f609e9e721d63214018015c0e8a566
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2023-09-15 13:32:28 +0200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,18 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res) => {
+app.use((req, res, next) => {
+    console.log('Requête reçue !');
+    next();
+});
+
+app.use((req, res, next) => {
+    res.status(201);
+    next();
+});
+
+app.use((req, res, next) => {
     res.json({message: 'votre requête a bien été reçue !'});
-})
+});
 
 module.exports = app;
