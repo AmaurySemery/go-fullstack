60047bdc48fd47df5d3cbf957c8712d08047d727
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2023-09-15 22:51:54 +0200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -32,6 +32,12 @@ app.post('/api/stuff', (req, res, next) => {
       next();
   });
 
+  app.delete('/api/stuff/:id', (req, res, next) => {
+    Thing.deleteOne({ _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+      .catch(error => res.status(400).json({ error }));
+  });
+
   app.put('/api/stuff/:id', (req, res, next) => {
     Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
       .then(() => res.status(200).json({ message: 'Objet modifié !'}))
